761e4e6b26496accdb7edc25370796827e07ca85
msam13 <msamridha16@gmail.com>
2024-04-20 13:37:30 -0500

    changes

--- duster.sh
+++ duster.sh
@@ -5,8 +5,8 @@ mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init zsh
+init bash
+init zsh
 git clone --recursive https://github.com/naver/dust3r
 cd dust3r
 conda create -n dust3r python=3.11 cmake=3.14.0
